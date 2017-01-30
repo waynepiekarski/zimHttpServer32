70cc7387c473661c531cb5af90d3464b96022f99
Wayne Piekarski <wayne@tinmith.net>
2017-01-29 20:19:51 -0800

    Implement init.d script to start up on OpenWRT systems

--- start-wiki.sh
+++ start-wiki.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-cd `dirname $0`
+cd /128gb
 
+echo "For some reason we get a lot of crashes initially, something is wrong with the kernel?"
 while [ 1 ]; do
   ./zimHttpServer.pl wikipedia_en_all_2016.zim
   echo "Restarted after failure ..."
